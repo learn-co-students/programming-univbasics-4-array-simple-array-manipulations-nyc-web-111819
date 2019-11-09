a22f0c9a1cd196f414172328e542fd905eabe050
Jay Wen <jaywen89@gmail.com>
2019-11-09 17:17:44 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,6 +23,25 @@ def shift_with_args (ice_cream_brands)
 end
 
 def using_concat (my_favorite_things, more_favs)
-  all_my_favs (my_favorite_things, more_favs)
-  all_my_favs.concat
+  (my_favorite_things).concat(more_favs)
+end
+
+def using_insert (list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
+end
+
+def using_uniq (haircuts)
+  haircuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors, name)
+  instructors.delete(name)
+end
+
+def using_delete_at (famous_robots, deleted_robot)
+  famous_robots.delete_at(2)
 end
